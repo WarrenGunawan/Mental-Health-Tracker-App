e370c14d7acf3651a03e39741a21a6cf701e791b
Warren Gunawan <warrengunawan@Warrens-MacBook-Air-2.local>
2026-01-05 14:50:48 -0800

    Set up persistence

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 
@@ -16,7 +17,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
-
-
-export { auth };
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
