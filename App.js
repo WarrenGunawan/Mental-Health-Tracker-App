26404b05efd2bb64212b7136165ceaf191f43a88
Warren Gunawan <warrengunawan@Warrens-MacBook-Air-2.local>
2026-01-05 14:49:25 -0800

    Reconstructed Stack Screens

--- App.js
+++ App.js
@@ -10,20 +10,45 @@ import AvatarPage from "./Screens/MainScreens/AvatarPage";
 // eslint-disable-next-line import/no-unresolved
 import EntryList from "./Screens/MainScreens/EntryList";
 
+import { AuthProvider, useAuth } from "./AuthContext";
+
+
 const Stack = createNativeStackNavigator();
+
+function AuthStack() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name='login' component={LoginPage} ></Stack.Screen>
+      <Stack.Screen name='signup' component={SignUpPage} ></Stack.Screen>
+    </Stack.Navigator>
+  )
+}
+
+function AppStack() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name='avatarpage' component={AvatarPage} ></Stack.Screen>
+      <Stack.Screen name='entrylist' component={EntryList} options={{ animation: 'slide_from_left' }}></Stack.Screen>
+    </Stack.Navigator>
+  )
+}
+
+function RootNavigator() {
+  const { user, authReady } = useAuth();
+
+  if (!authReady) return null; 
+
+  return user ? <AppStack /> : <AuthStack />;
+}
 
 
 export default function App() {
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}> 
-        <Stack.Screen name='login' component={LoginPage} ></Stack.Screen>
-        <Stack.Screen name='signup' component={SignUpPage} ></Stack.Screen>
-
-        <Stack.Screen name='avatarpage' component={AvatarPage} ></Stack.Screen>
-        <Stack.Screen name='entrylist' component={EntryList} options={{ animation: 'slide_from_left' }}></Stack.Screen>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AuthProvider>
+      <NavigationContainer>
+        <RootNavigator/>
+      </NavigationContainer>
+    </AuthProvider>
   );
 }
