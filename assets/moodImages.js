c4dd1f9332dab7a01eef57e450dd62e880128cdd
Warren Gunawan <warrengunawan@Warrens-MacBook-Air-2.local>
2026-01-05 14:51:24 -0800

    Exported monkey images

---
+++ assets/moodImages.js
@@ -0,0 +1,18 @@
+import happiest from '../assets/images/happiest.jpeg';
+import happy from '../assets/images/happy.jpeg';
+import neutral from '../assets/images/neutral.jpeg';
+import sad from '../assets/images/sad.jpeg';
+import saddest from '../assets/images/saddest.jpg';
+import thinking from '../assets/images/thinking.jpg';
+
+
+const moodImages = [
+    { id: 'happiest', image: happiest },
+    { id: 'happy', image: happy },
+    { id: 'neutral', image: neutral },
+    { id: 'sad', image: sad },
+    { id: 'saddest', image: saddest },
+    { id: 'thinking', image: thinking }
+];
+
+export default moodImages;
