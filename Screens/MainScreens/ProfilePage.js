4bc8df0999d65e8cda54da1c76f64b4efe2791a8
Warren Gunawan <warrengunawan@ucr-secure-39-10-13-204-142.wnet.ucr.edu>
2026-01-23 10:40:37 -0800

    Edited Layout

--- Screens/MainScreens/ProfilePage.js
+++ Screens/MainScreens/ProfilePage.js
@@ -8,6 +8,9 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import TopBar from '../../Components/TopBar';
 
+import Wooper from '../../assets/images/wooper.png';
+
+
 
 const ProfilePage = () => {
 
@@ -19,7 +22,15 @@ const ProfilePage = () => {
 
 
             <View style={styles.profileContainer}>
-                <Text>Profile</Text>
+                <Image source={Wooper} style={styles.pfp}/>
+                <Text style={styles.username}>Super Funny Username</Text>
+                <Text style={styles.name}>Warren Gunawan</Text>
+                <View style={styles.informationContainer}> 
+                    <Text style={styles.aboutMeTitle}>About Me!</Text>
+                    <View>
+                        <Text style={styles.aboutMe}>Placeholder text, this is all about me. </Text>
+                    </View>
+                </View>
             </View>
         </SafeAreaView>
     )
@@ -36,14 +47,57 @@ const styles = StyleSheet.create({
     profileContainer: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center',
 
         marginTop: 35,
         width: '80%',
-
-        backgroundColor: 'rgba(0,0,0,0.1)',
-
         borderRadius: 40,
+
+        backgroundColor: '#DDDDDD',
+    },
+
+    pfp: {
+        width: 150,
+        height: 150,
+
+        borderRadius: 200,
+        backgroundColor: '#BBBBBB',
+        marginTop: 20,
+    },
+
+    username: {
+        fontSize: 25,
+        marginTop: 10,
+    },
+
+    name: {
+        fontSize: 15,
+        color: '#666666'
+    },
+
+    informationContainer: {
+        flex: 1,
+        justifyContent: 'center',
+
+        backgroundColor: '#BBBBBB',
+        borderRadius: 20,
+
+        marginTop: 10,
+        
+        width: '80%',
+        marginBottom: 30,
+    },
+
+    aboutMeTitle: {
+        fontSize: 15,
+        padding: 20,
+
+        color: '#666666'
+    },
+
+    aboutMe: {
+        fontSize: 15,
+        paddingHorizontal: 20,
+        paddingBottom: 20,
     },
 })
 
