baa459ab4332ec5bca3e1f1d4a1b39951ab7e326
Warren Gunawan <warrengunawan@ucr-secure-39-10-13-204-150.wnet.ucr.edu>
2026-01-27 16:54:16 -0800

    Added components

--- Screens/MainScreens/ProfilePage.js
+++ Screens/MainScreens/ProfilePage.js
@@ -2,36 +2,30 @@ import { Text, View, Image, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useEffect, useState } from 'react';
 import { LinearGradient } from 'expo-linear-gradient';
+import { useRoute } from '@react-navigation/core';
 import * as ImagePicker from 'expo-image-picker';
 import * as FileSystem from 'expo-file-system';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import TopBar from '../../Components/TopBar';
+import ProfileView from '../../Components/ProfileView';
+import EditableProfileView from '../../Components/EditableProfileView';
 
-import Wooper from '../../assets/images/wooper.png';
 
 
 
 const ProfilePage = () => {
 
+    const route = useRoute();
+
+    const mode = route.params?.mode ?? 'view';
 
     return (
         <SafeAreaView style={styles.container}>
             <TopBar />
             <LinearGradient colors={['#FFFFFF', 'transparent']} start={{ x: 0, y: 0 }} end={{ x: 0, y: 0.04 }} style={{ height: 5, width: '100%' }} />
 
-
-            <View style={styles.profileContainer}>
-                <Image source={Wooper} style={styles.pfp}/>
-                <Text style={styles.username}>Super Funny Username</Text>
-                <Text style={styles.name}>Warren Gunawan</Text>
-                <View style={styles.informationContainer}> 
-                    <Text style={styles.aboutMeTitle}>About Me!</Text>
-                    <View>
-                        <Text style={styles.aboutMe}>Placeholder text, this is all about me. </Text>
-                    </View>
-                </View>
-            </View>
+            {mode === 'edit' ? <EditableProfileView /> : <ProfileView />}
         </SafeAreaView>
     )
 }
@@ -42,62 +36,6 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-    },
-
-    profileContainer: {
-        flex: 1,
-        alignItems: 'center',
-
-        marginTop: 35,
-        width: '80%',
-        borderRadius: 40,
-
-        backgroundColor: '#DDDDDD',
-    },
-
-    pfp: {
-        width: 150,
-        height: 150,
-
-        borderRadius: 200,
-        backgroundColor: '#BBBBBB',
-        marginTop: 20,
-    },
-
-    username: {
-        fontSize: 25,
-        marginTop: 10,
-    },
-
-    name: {
-        fontSize: 15,
-        color: '#666666'
-    },
-
-    informationContainer: {
-        flex: 1,
-        justifyContent: 'center',
-
-        backgroundColor: '#BBBBBB',
-        borderRadius: 20,
-
-        marginTop: 10,
-        
-        width: '80%',
-        marginBottom: 30,
-    },
-
-    aboutMeTitle: {
-        fontSize: 15,
-        padding: 20,
-
-        color: '#666666'
-    },
-
-    aboutMe: {
-        fontSize: 15,
-        paddingHorizontal: 20,
-        paddingBottom: 20,
     },
 })
 
