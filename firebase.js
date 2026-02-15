d088b0602e9c757137be72790d24da853c6a0f2a
Warren Gunawan <warrengunawan@Warrens-MacBook-Air-2.local>
2026-02-14 18:48:48 -0800

    Changed to work with storage

--- firebase.js
+++ firebase.js
@@ -1,25 +1,30 @@
-import { initializeApp } from 'firebase/app';
-import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { getFirestore } from 'firebase/firestore';
-
-
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { initializeAuth, getAuth, getReactNativePersistence } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD49y7PR0x1CTfHVLyRBY1kE79-eKHDEbA",
   authDomain: "mental-health-tracker-innerhue.firebaseapp.com",
   projectId: "mental-health-tracker-innerhue",
-  storageBucket: "mental-health-tracker-innerhue.firebasestorage.app",
+  storageBucket: "mental-health-tracker-innerhue.appspot.com",
   messagingSenderId: "223782544936",
   appId: "1:223782544936:web:f39ad807b40d8a0456f71d",
-  measurementId: "G-Z9P1MK83V1"
+  measurementId: "G-Z9P1MK83V1",
 };
 
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
-const app = initializeApp(firebaseConfig);
+let auth;
+try {
+  auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage),
+  });
+} catch (e) {
+  auth = getAuth(app);
+}
 
-export const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage),
-});
-
+export { app, auth };
 export const db = getFirestore(app);
+export const storage = getStorage(app);
